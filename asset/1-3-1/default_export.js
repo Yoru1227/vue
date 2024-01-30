a47f53a169c170cc7dc6557e62c31d3989d55737
Yoru <Nokersx@gmail.com>
2024-01-30 16:31:01 +0800

    first commit

---
+++ asset/1-3-1/default_export.js
@@ -0,0 +1,13 @@
+const someone = {
+  name: "Amy",
+  age: 18,
+  sayHi,
+};
+
+function sayHi() {
+  console.log("Hello world!");
+}
+
+const x = 3;
+
+export default someone;
---
+++ asset/1-3-1/mixed_export.js
@@ -0,0 +1,14 @@
+const someone = {
+  name: "Amy",
+  age: 18,
+  sayHi,
+};
+
+function sayHi() {
+  console.log("Hello world!");
+}
+
+const x = 3;
+
+export default someone;
+export { sayHi, x };
